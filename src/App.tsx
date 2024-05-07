035408147e66f1d0d2ad29642c9d4b86b88db94b
x417v <ilyakinskiy@gmail.com>
2024-05-07 19:28:31 +0300

    fix slider img width

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import SliderPage from 'components/assets/Slider/Slider';
 import Header from 'components/modules/Header/Header';
 
 function App() {
   return (
     <div>
       <Header />
+      <SliderPage />
     </div>
   );
 }
--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,5 +1,4 @@
 import styles from 'styles/header/index.module.scss';
-import SliderPage from 'components/assets/Slider/Slider';
 import CartSvg from 'components/elements/CartSvg/CartSvg';
 import FavSvg from 'components/elements/FavSvg/FavSvg';
 import Logo from 'components/elements/Logo/Logo';
@@ -9,35 +8,29 @@ import VoiceSvg from 'components/elements/VoiceSvg/VoiceSvg';
 
 const Header = () => {
   return (
-    <>
-      <header className={styles.header}>
-        <Logo color={'#000000'} />
-        <nav className={styles.header__nav}>
-          <ul>
-            <li>Каталог</li>
-            <li>Оплата та доставка</li>
-            <li>Про нас</li>
-          </ul>
-        </nav>
-        <div className={styles.header__right}>
-          <form>
-            <VoiceSvg />
-            <input type="text" />
-            <SearchSvg />
-          </form>
-          <div className={styles.header__right__actions}>
-            <span>UA/EN</span>
-            <FavSvg />
-            <ProfileSvg />
-            <CartSvg />
-          </div>
+    <header className={styles.header}>
+      <Logo color={'#000000'} />
+      <nav className={styles.header__nav}>
+        <ul>
+          <li>Каталог</li>
+          <li>Оплата та доставка</li>
+          <li>Про нас</li>
+        </ul>
+      </nav>
+      <div className={styles.header__right}>
+        <form>
+          <VoiceSvg />
+          <input type="text" />
+          <SearchSvg />
+        </form>
+        <div className={styles.header__right__actions}>
+          <span>UA/EN</span>
+          <FavSvg />
+          <ProfileSvg />
+          <CartSvg />
         </div>
-      </header>
-
-      <div className="">
-        <SliderPage />
       </div>
-    </>
+    </header>
   );
 };
 
