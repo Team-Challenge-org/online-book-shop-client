e03eb9c34a62dc5a6e17ace46082bc70c012865b
x417v <ilyakinskiy@gmail.com>
2024-05-18 18:15:40 +0300

    Add image to slider from backend

--- src/components/assets/Slider/SliderItem.tsx
+++ src/components/assets/Slider/SliderItem.tsx
@@ -1,10 +1,11 @@
 import styles from 'styles/slider/index.module.scss';
 import { SliderItemType } from 'types/commont';
+import SliderImage from './SliderImage';
 
 const SliderItem = ({ obj }: SliderItemType) => {
   return (
     <div className={styles.slider}>
-      <img src="https://picsum.photos/3001/568" className={styles.slider__image} alt={obj.title} />
+      <SliderImage data={obj.titleImage} title={obj.title} />
       <h1 className={styles.slider__title}>{obj.title}</h1>
       <button className={styles.slider__button}>Детальніше</button>
     </div>
