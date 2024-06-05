dc46a0240fe4dbbc163920ca9b32eb4fe0ca21c0
x417v <ilyakinskiy@gmail.com>
2024-06-05 22:59:29 +0300

    Continued styled slider in productPage

--- src/components/modules/ProductPage/ProductPageSlider/ProductPageSlider.tsx
+++ src/components/modules/ProductPage/ProductPageSlider/ProductPageSlider.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import Slider from 'react-slick';
 import styles from 'styles/productPage/index.module.scss';
 
+function SampleNextArrow(props: any) {
+  return (
+    <div {...props} className={styles.product__main__slider__arrow_next__wrapper}>
+      <div {...props} className={styles.product__main__slider__arrow_next} />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props: any) {
+  return (
+    <div {...props} className={styles.product__main__slider__arrow_prev__wrapper}>
+      <div {...props} className={styles.product__main__slider__arrow_prev} />
+    </div>
+  );
+}
+
 const ProductPageSlider = ({ images }: any) => {
   const settings = {
     infinite: true,
@@ -11,6 +27,8 @@ const ProductPageSlider = ({ images }: any) => {
     autoplay: true,
     autoplaySpeed: 3000,
     cssEase: 'linear',
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   return (
