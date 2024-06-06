ffb43d5aa01d54e602f4babaa1b31c3a7d3c6b6d
x417v <ilyakinskiy@gmail.com>
2024-06-06 20:05:46 +0300

    Add slider image to product page. Styled slider in RecentlViewed

--- src/components/modules/ProductPage/recentlyViewedBooks/RecentlyViewedBooks.tsx
+++ src/components/modules/ProductPage/recentlyViewedBooks/RecentlyViewedBooks.tsx
@@ -4,40 +4,53 @@ import {
 } from "utils/truncateString";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { MdArrowForwardIos } from "react-icons/md";
-import { MdArrowBackIosNew } from "react-icons/md";
 import { selectRecentlyViewedBooks } from "store/recentlyViewedBooks/selectors";
-
 import styles from "./recentlyViewedBooks.module.scss";
-
 import Slider from "react-slick";
+
+
+function SampleNextArrow(props: any) {
+  return (
+    <div {...props} className={styles.recently__arrow_next__wrapper}>
+      <div {...props} className={styles.recently__arrow_next} />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props: any) {
+  return (
+    <div {...props} className={styles.recently__arrow_prev__wrapper}>
+      <div {...props} className={styles.recently__arrow_prev} />
+    </div>
+  );
+}
+
 
 export function RecentlyViewedBooks() {
   const navigate = useNavigate();
   const { books: recentlyViewedBooks } = useSelector(selectRecentlyViewedBooks);
 
-  const settings = {
+  const settingsProduct = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
-    nextArrow: <MdArrowForwardIos />,
-    prevArrow: <MdArrowBackIosNew />,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
 
   return recentlyViewedBooks.length === 0 ? null : (
-    <section>
+    <section className={styles.recently}>
       <h2>Нещодавно переглянуті книги</h2>
 
-      <ul>
-        <Slider {...settingsProduct}>
+      <ul className={styles.recently__list}>
+        <Slider {...settingsProduct} className={styles.book_box__slider}>
           {recentlyViewedBooks?.map((book) => (
             <li key={book.id} className={styles.book_box}>
               <img
                 src={book?.titleImage as string}
-                alt={"image of: " + book.title}
-                style={{ width: "302px", height: "368px" }}
+                alt={"image of: " + book.title} className={styles.book_box__image}
               />
 
               <div className={styles.text_info}>
