ed7909d2f35ed254087e64f6eb5a0f519d3205f8
Samstas <ssfrontendd@gmail.com>
2024-06-08 17:29:46 +0200

    refactor section similar books with new slider

--- src/components/modules/ProductPage/similarBooks/SimilarBooks.tsx
+++ src/components/modules/ProductPage/similarBooks/SimilarBooks.tsx
@@ -2,80 +2,82 @@ import {
   handleTruncateAuthors,
   handleTruncateBookTitle,
 } from "utils/truncateString";
+import { useCallback } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { setSimilarBooks } from "store/books/booksSlice";
+import { EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
 import { selectSimilarBooks } from "store/books/selectors";
-// import Slider from "react-slick";
 
+import { MdArrowBackIosNew } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
 import styles from "./similarBooks.module.scss";
 
-// function SampleNextArrow(props: any) {
-//   return (
-//     <div {...props} className={styles.recently__arrow_next__wrapper}>
-//       <div {...props} className={styles.recently__arrow_next} />
-//     </div>
-//   );
-// }
-
-// function SamplePrevArrow(props: any) {
-//   return (
-//     <div {...props} className={styles.recently__arrow_prev__wrapper}>
-//       <div {...props} className={styles.recently__arrow_prev} />
-//     </div>
-//   );
-// }
-
 export function SimilarBooks() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   const similarBooks = useSelector(selectSimilarBooks);
+  const navigate = useNavigate();
 
-  //   const settingsProduct = {
-  //     dots: false,
-  //     infinite: true,
-  //     speed: 500,
-  //     slidesToShow: 4,
-  //     slidesToScroll: 1,
-  //     nextArrow: <SampleNextArrow />,
-  //     prevArrow: <SamplePrevArrow />,
-  //   };
+  const options: EmblaOptionsType = {
+    loop: true,
+    align: "start",
+  };
 
-  return similarBooks.length === 0 ? null : (
-    <section className={styles.recently}>
-      <h2>Нещодавно переглянуті книги</h2>
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
 
-      <ul className={styles.recently__list}>
-        {/* <Slider {...settingsProduct} className={styles.book_box__slider}> */}
-        {similarBooks?.map((book) => (
-          <li key={book.id} className={styles.book_box}>
-            <img
-              src={book?.titleImage as string}
-              alt={"image of: " + book.title}
-              className={styles.book_box__image}
-            />
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
-            <div className={styles.text_info}>
-              <p data-title={book.authors as string}>
-                {handleTruncateAuthors(book?.authors as string)}
-              </p>
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
-              <h3
-                data-title={book.title}
-                onClick={() => {
-                  navigate(`/book/${book.id}`);
-                  dispatch(setSimilarBooks(book));
-                }}
-              >
-                {handleTruncateBookTitle(book?.title)}
-              </h3>
+  return (
+    <section>
+      <h2>Вас може зацікавити</h2>
 
-              <span>{book.price} грн</span>
-            </div>
-          </li>
-        ))}
-        {/* </Slider> */}
-      </ul>
+      <div className={styles.slider_embla}>
+        <MdArrowBackIosNew
+          className={styles.embla__prev}
+          onClick={scrollPrev}
+        />
+
+        <div className={styles.embla}>
+          <div className={styles.sembla__viewport} ref={emblaRef}>
+            <ul className={styles.embla__container}>
+              {similarBooks.map((book) => (
+                <li key={book.id} className={styles.embla__slide}>
+                  <img
+                    src={book?.titleImage as string}
+                    alt={"image of: " + book.title}
+                    className={styles.book_image}
+                  />
+
+                  <div className={styles.text_info_box}>
+                    <p data-title={book.authors as string}>
+                      {handleTruncateAuthors(book?.authors as string)}
+                    </p>
+
+                    <h3
+                      data-title={book.title}
+                      onClick={() => navigate(`/book/${book.id}`)}
+                    >
+                      {handleTruncateBookTitle(book?.title)}
+                    </h3>
+
+                    <span>{book.price} грн</span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <MdArrowForwardIos
+          className={styles.embla__next}
+          onClick={scrollNext}
+        />
+      </div>
     </section>
   );
 }
