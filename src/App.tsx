ba5a6cb1c1687bff3b03c3067181ceaed70b3b8f
x417v <ilyakinskiy@gmail.com>
2024-06-24 19:27:28 +0300

    Create errors pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import MainLayout from 'components/layouts/MainLayout';
+import NotFoundPage from 'pages/ErrorPage/NotFoundPage';
+import ServiceUnavailablePage from 'pages/ErrorPage/ServiceUnavailablePage';
 import HomePage from 'pages/HomePage/HomePage';
 import LoginPageLogic from 'pages/LoginPage/LoginPageLogic';
 import ProductPage from 'pages/ProductPage/ProductPage';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,6 +13,11 @@ function App() {
         <Route path="" element={<HomePage />} />
         <Route path="book/:id" element={<ProductPage />} />
         <Route path="login" element={<LoginPageLogic />} />
+
+        {/* Errors: */}
+        <Route path="/404" element={ <NotFoundPage /> } />
+        <Route path="/503" element={ <ServiceUnavailablePage /> } />
+        <Route path="*" element={ <Navigate to="/404" replace />} />
       </Route>
     </Routes>
   );
