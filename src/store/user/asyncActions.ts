bd5cf10ce0af4c1ed7f1792fe70704702043def3
x417v <ilyakinskiy@gmail.com>
2024-06-27 10:42:53 +0300

    Change url to new server. Add .env to gitignore

--- src/store/user/asyncActions.ts
+++ src/store/user/asyncActions.ts
@@ -1,10 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { TUser } from './types';
+import { Endpoints } from 'constants/api';
 
 export const loginUser = createAsyncThunk('auth/login', async (userCrentials: TUser) => {
-  const { data } = await axios.post(
-    'https://quiet-ocean-77925-b4d85148e93b.herokuapp.com/api/v1/auth/login',
+  const { data } = await axios.post(Endpoints.LOGIN,
     userCrentials,
   );
   localStorage.setItem('user', JSON.stringify(data));
