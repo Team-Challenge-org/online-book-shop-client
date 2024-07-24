1e5c9b6b6f79548eefb91ff8e8d4ef763dcbf248
x417v <ilyakinskiy@gmail.com>
2024-07-24 22:49:55 +0300

    Work with payment form

--- src/components/modules/OrderPage/PaymentForm/PaymentForm.tsx
+++ src/components/modules/OrderPage/PaymentForm/PaymentForm.tsx
@@ -1,16 +1,75 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import React from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import { Controller, FormProvider, useForm, useFormContext } from 'react-hook-form';
 import { orderContactsSchema, TOrderContactsSchema } from 'validations/orderContactsSchema';
 import styles from '../orderPage.module.scss';
 
 export default function PaymentForm() {
+  //const {
+  //  register,
+  //  formState: { errors },
+  //} = useFormContext();
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      payment: '',
+    },
+  });
+
   const methods = useForm<TOrderContactsSchema>({
     resolver: zodResolver(orderContactsSchema),
   });
   return (
     <FormProvider {...methods}>
       <span className={styles.order__subtitle}>Оплата</span>
+
+      {/*<Controller
+        name="payment"
+        control={control}
+        defaultValue={'Оплата карткою Online'}
+        render={({ field }) => (
+          <>
+            <input
+              {...field}
+              type="radio"
+              value={'Оплата карткою Online'}
+              name="online"
+              id="payment"
+            />
+            <input
+              {...field}
+              type="radio"
+              value={'Готівкою або карткою: при отриманні'}
+              name="cash"
+              id="payment"
+            />
+          </>
+        )}
+      />*/}
+
+      {/*<Controller
+        name="payment"
+        control={control}
+        render={({ field }) => (
+          <input
+            {...field}
+            type="radio"
+            value={'Готівкою або карткою: при отриманні'}
+            name="cash"
+            id="payment"
+          />
+        )}
+      />*/}
+
+      <label id="field-online">
+        <input id="field-online" {...register('payment')} type="radio" value="online" checked />
+        Оплата карткою Online
+      </label>
+
+      <label id="field-cash">
+        <input id="field-cash" {...register('payment')} type="radio" value="cash" />
+        Готівкою або карткою: при отриманні
+      </label>
     </FormProvider>
   );
 }
