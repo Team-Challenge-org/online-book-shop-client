a1ab88d4026acb2a4568c2eccd8bf44d1aeb3a2c
Illia Kinskyi <ilyakinskiy@gmail.com>
2024-08-25 22:41:24 +0300

    Fix bugs with token. Continued working with profilePersonal

--- src/components/modules/ProfilePage/PersonalPage/PersonalPage.tsx
+++ src/components/modules/ProfilePage/PersonalPage/PersonalPage.tsx
@@ -1,9 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import axios from 'axios';
-import Cookies from 'js-cookie';
 import React, { useEffect, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-import axiosConfig from 'utils/axiosConfig';
 import { firstNameSchema, TFirstNameSchema } from 'validations/profileSchema';
 import PersonalPageInput from './PersonalPageInput';
 import { selectUserData } from 'store/user/selectors';
@@ -11,32 +8,68 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from 'store/store';
 import { getUser } from 'store/user/asyncActions';
 import Spinner from 'components/elements/Spinner/Spinner';
+import styles from '../profilePage.module.scss';
+import { TRegisterField } from 'types/auth';
+
+const registerFields: TRegisterField[] = [
+  {
+    id: 1,
+    type: 'text',
+    label: 'Ваше ім’я *',
+    placeholder: 'Введіть ваше ім’я',
+    valueName: 'first_name',
+    errorTips: [
+      'Ви можете використовувати лише кирилицю, латиницю та арабські цифри.',
+      'Ви можете використовувати великі та малі літери.',
+      'Довжина імені має бути від 2 до 30 символів.',
+    ],
+  },
+  {
+    id: 2,
+    type: 'text',
+    label: 'Ваше прізвище *',
+    placeholder: 'Введіть ваше прізвище',
+    valueName: 'last_name',
+    errorTips: [
+      'Ви можете використовувати лише кирилицю, латиницю та арабські цифри.',
+      'Ви можете використовувати великі та малі літери.',
+      'Довжина імені має бути від 2 до 50 символів.',
+    ],
+  },
+  {
+    id: 3,
+    type: 'number',
+    label: 'Номер телефону *',
+    placeholder: '+38',
+    valueName: 'phone_number',
+    errorTips: [
+      'Ви можете використовувати лише арабські цифри та «+».',
+      'Довжина мобільного номера має бути 13 символів, включаючи «+».',
+    ],
+  },
+  {
+    id: 4,
+    type: 'email',
+    label: 'Електронна пошта *',
+    placeholder: 'Введіть електронну пошту',
+    valueName: 'email',
+    errorTips: [
+      "Ви можете використовувати лише арабські цифри, латиницю та наступні символи ~ ! $ % ^ & * _ = + } { ' ? - @.",
+      'Ви можете використовувати великі та малі літери.',
+      'Пошта має містити “@”.',
+      "Пошта повинна мати будь-який діючий домейн окрім “mail.ru”, “yandex.ru” та інших доменів, пов'язаних з росією.",
+    ],
+  },
+];
 
 export default function PersonalPage() {
   const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState(null);
   const user = useSelector(selectUserData);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     dispatch(getUser());
   }, []);
-
-  console.log(user);
-
-  //useEffect(() => {
-  //  const getData = async () => {
-  //    const { data } = await axiosConfig.get('/api/v1/profile/user');
-
-  //    console.log(data);
-
-  //    setFirstName(data.firstName);
-  //    return data;
-  //  };
-
-  //  getData();
-  //}, []);
 
   const methods = useForm<TFirstNameSchema>({
     resolver: zodResolver(firstNameSchema),
@@ -57,7 +90,7 @@ export default function PersonalPage() {
   }, [watchName, firstName]);
 
   return (
-    <div>
+    <div className={styles.profile__personal}>
       {user.loading ? (
         <Spinner />
       ) : (
@@ -70,7 +103,7 @@ export default function PersonalPage() {
               </label>
             </form>
           </FormProvider>
-          <PersonalPageInput lastName={user.lastName} />
+          <PersonalPageInput field={registerFields[1]} />
         </>
       )}
     </div>
