a4f9a3bc2929e115a539bee4c1d9736ef40c79e8
Illia Kinskyi <ilyakinskiy@gmail.com>
2024-09-06 20:41:37 +0300

    Fix: bug, showing empty cart in order page

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,9 +1,27 @@
-import OrderForms from "components/modules/OrderPage/OrderForms/OrderForms";
-import styles from "../../components/modules/OrderPage/orderPage.module.scss";
-import OrderItems from "components/modules/OrderPage/OrderItems/OrderItems";
-import OrderConfirm from "components/modules/OrderPage/OrderConfirm/OrderConfirm";
+import OrderForms from 'components/modules/OrderPage/OrderForms/OrderForms';
+import styles from '../../components/modules/OrderPage/orderPage.module.scss';
+import OrderItems from 'components/modules/OrderPage/OrderItems/OrderItems';
+import OrderConfirm from 'components/modules/OrderPage/OrderConfirm/OrderConfirm';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from 'store/auth/selectors';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { selectAuthUserCart } from 'store/cart/selectors';
 
 export default function OrderPage() {
+  const isAuth = useSelector(selectIsAuth);
+  const cart = useSelector(selectAuthUserCart);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuth) {
+      navigate('/');
+    }
+    if (cart.cartItems.length < 1) {
+      navigate('/');
+    }
+  }, [isAuth, cart]);
+
   return (
     <main className={styles.order}>
       <section className={styles.order__block}>
